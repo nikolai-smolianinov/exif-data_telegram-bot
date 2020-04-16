28774e84a424a24a23aa33b99098a59a568d790c
niko_lie@mail.ru <Melbravo544611>
2020-04-16 21:19:41 +0300

    handling wrong message types. multi-language responses

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import json
 import telebot
 from telebot import apihelper
 
+from replies import replies
+
 with open('config.json', 'r') as f:
     config = json.load(f)
 
@@ -12,8 +14,32 @@ apihelper.proxy = {'https':'socks5://{}:{}@{}:{}'.format(proxy['user'], proxy['p
 
 bot = telebot.TeleBot(TG_API_TOKEN)
 
+def sendTextMessage(message, text):
+	bot.send_message(message.chat.id, text, parse_mode="HTML")
+
+def handleWelcomeMessage(message):
+	lang = 'en'
+	if message.from_user.language_code == 'ru':
+		lang = 'ru'
+
+	sendTextMessage(message, replies['welcome'][lang])
+
+def replyToPhoto(message):
+	sendTextMessage(message, 'coming soon...')
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.send_message(message.chat.id, "Greetings! This bot will help you get some data from your image!\n\n<s>as if someone need it</s>", parse_mode='HTML')
+	handleWelcomeMessage(message)
+
+@bot.message_handler(content_types=['text', 'audio', 'document', 'photo', 'sticker', 'video', 'video_note', 'voice', 'location', 'contact', 'new_chat_members', 'left_chat_member', 'new_chat_title', 'new_chat_photo', 'delete_chat_photo', 'group_chat_created', 'supergroup_chat_created', 'channel_chat_created', 'migrate_to_chat_id', 'migrate_from_chat_id', 'pinned_message'])
+def echo_all(message):
+	lang = 'en'
+	if message.from_user.language_code == 'ru':
+		lang = 'ru'
+	content_type = message.content_type
+	if content_type == 'photo':
+		replyToPhoto(message)
+		return
+	sendTextMessage(message, replies['noPhoto'][lang])
 
 bot.polling()
---
+++ replies.py
@@ -0,0 +1,10 @@
+replies = {
+	'welcome': {
+		'ru': 'Здравствуйте! Этот бот может сообщить вам некоторые данные из ваших фотографий!\n\n <s>Круто же : (</s>',
+		'en': '"Greetings! This bot will help you get some data from your image!\n\n<s>as if someone need it</s>'
+	},
+	'noPhoto':{
+		'ru': 'Cорри, но нужно отправить именно <strong>фото</strong>!',
+		'en': 'Apologies, but I can only process <strong>photos</strong>.'
+	}
+}			
